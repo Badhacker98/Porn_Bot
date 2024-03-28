ef974bea78cf516b27e05abb770ee198d5a000b0
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-03-28 17:07:36 +0530

    Update and rename Porn.py to porn.py

--- Porn/modules/porn.py
+++ Porn/modules/porn.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 import requests, random
 from bs4 import BeautifulSoup
-from Porn import porn
+from Porn import *
 import pytgcalls
 import os, yt_dlp 
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
