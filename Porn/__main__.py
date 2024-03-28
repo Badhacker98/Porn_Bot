b79b94a8c70356cacfaab55074b957f033a9a5db
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-03-28 16:36:32 +0530

    Update __main__.py

--- Porn/__main__.py
+++ Porn/__main__.py
@@ -4,18 +4,18 @@ from pyrogram import idle
 from Porn import Porn
 from Porn.bad import ALL_MODULES
 
- 
+LOGGER_ID = -1002056907061
 
 loop = asyncio.get_event_loop()
-
 
 async def bad_boot():
     for all_module in ALL_MODULES:
         importlib.import_module("Porn.bad." + all_module)
-    print("»»»» ʜᴇʀᴏᴋᴏ ʀᴏʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ ✨ 🎉")
+    print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝗌𝗍𝖺𝗋𝗍")
     await idle()
-    print("»» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
-
+    print("𝖻𝖺𝗁𝖺𝗇𝖼𝗁𝗈𝖽 𝖯𝗂𝗋𝗈 𝖢𝗈𝖽𝖾𝗋 𝗄𝗋𝗅𝗈 𝖾𝖽𝗂𝗍 𝖺𝖺 𝗀𝗒𝖺 𝗇 𝖾𝗋𝗋𝗈𝗋 𝖺𝖺𝖻 𝗃𝖺𝗄𝖾 𝗀𝖺𝗇𝖽 𝗆𝖺𝗋𝗐𝖺𝗈 𝗂𝗌𝗌𝖾 @iam_daxx")
+    await Porn.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [♡³_🫧𝆺꯭𝅥˶֟፝͟͝β𝝰꯭‌𝞉 ꯭𝝡꯭𝞄꯭𝞌𝞉꯭𝝺꯭𝆺꯭𝅥🍷┼❤️//](https://t.me/ll_BAD_BBY_ll)**")
 
 if __name__ == "__main__":
     loop.run_until_complete(bad_boot())
+    
