45b56f99216a0024462e01f96ede1e698372544a
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-03-28 16:41:53 +0530

    Update __main__.py

--- Porn/__main__.py
+++ Porn/__main__.py
@@ -2,7 +2,7 @@ import asyncio
 import importlib
 from pyrogram import idle
 from Porn import Porn
-from Porn.bad import ALL_MODULES
+from Porn.modules import ALL_MODULES
 
 LOGGER_ID = -1002056907061
 
@@ -10,7 +10,7 @@ loop = asyncio.get_event_loop()
 
 async def bad_boot():
     for all_module in ALL_MODULES:
-        importlib.import_module("Porn.bad." + all_module)
+        importlib.import_module("Porn.modules." + all_module)
     print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝗌𝗍𝖺𝗋𝗍")
     await idle()
     print("𝖻𝖺𝗁𝖺𝗇𝖼𝗁𝗈𝖽 𝖯𝗂𝗋𝗈 𝖢𝗈𝖽𝖾𝗋 𝗄𝗋𝗅𝗈 𝖾𝖽𝗂𝗍 𝖺𝖺 𝗀𝗒𝖺 𝗇 𝖾𝗋𝗋𝗈𝗋 𝖺𝖺𝖻 𝗃𝖺𝗄𝖾 𝗀𝖺𝗇𝖽 𝗆𝖺𝗋𝗐𝖺𝗈 𝗂𝗌𝗌𝖾 @iam_daxx")
